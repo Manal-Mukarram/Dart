8aa61ee4d71f7efd03dcaa5776a4a941166a5f79
Manal Mukarram <manalmukarram18@gmail.com>
2025-01-04 17:45:34 +0530

    practised while and do while

--- Loops.dart
+++ Loops.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 void main() {
   // program for star patters using for loop (I'm improving :D)(You did this within 2-3 mins :clepsss)
   for (int i = 0; i <= 5; i++) {
     print('*' * i);
   }
+
   // program for multiplication tables from 1-5 using while loop
   var multiplicationTable = 5;
   var i = 0;
@@ -10,4 +13,29 @@ void main() {
     print('${multiplicationTable * i}');
     i++;
   }
+
+  // program for a user until they enter a correct password using while and do while loops
+  var enterUsername = 'Admin';
+  print('Enter Password here:');
+  String? enterPassword;
+  enterPassword = stdin.readLineSync();
+  while (enterUsername == 'Admin') {
+    if (enterPassword == 'Pass123') {
+      print('Welcome, Admin');
+      break;
+    } else {
+      print('Incorrect Password');
+      enterPassword = stdin.readLineSync();
+    }
+  }
+  print('--------------');
+  // do {
+  //   if (enterPassword == 'Pass123') {
+  //     print('Welcome, Admin');
+  //     break;
+  //   } else {
+  //     print('Incorrect Password');
+  //     enterPassword = stdin.readLineSync();
+  //   }
+  // } while (enterUsername == 'Admin');
 }
