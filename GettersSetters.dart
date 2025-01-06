5e26ae8a5be2a92b2bdf932403411e58cc2b2d04
Manal Mukarram <manalmukarram18@gmail.com>
2025-01-06 18:58:27 +0530

    practised getters and setters.. need a lil more practice

--- GettersSetters.dart
+++ GettersSetters.dart
@@ -1,24 +1,28 @@
-void main() {
-  /* program to create a Student class with the following:
+// import 'dart:async';
+/* program to create a Student class with the following:
 A private variable _name (String).
 A private variable _score (int).
 A getter to retrieve the student's name.
 A setter to set the student's score, ensuring it is between 0 and 100.
 Challenge: Try printing the name and score of a student after setting them. */
-  print(Student().studentName);
+
+void main() {
+  Student student = Student();
+  student.studentInfo = 100;
+  print(student.getStudentName);
+  print(student.getStudentScore);
 }
 
 class Student {
-  String _studentName = 'hello';
-  int? _studentScore;
-
-  String get studentName => _studentName;
-  set student(int value) {
-    if (studentName == 'Manal') {
-      _studentName =
-          studentName; // Update the variable only if the condition is met
-    } else {
-      print("You aren't our student");
-    }
+  final _studentName = 'Manal';
+  var _studentScore = 0;
+  String get getStudentName => _studentName;
+  int get getStudentScore => _studentScore;
+  void set studentInfo(int score) {
+    if (score >= 0 && score <= 100) {
+      _studentScore = score;
+      print('$_studentName score is $_studentScore');
+    } else
+      (print("Invalid score..."));
   }
 }
