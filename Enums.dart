8adbfeae8d00d6164e6286e31dcbee4f271e8742
Manal Mukarram <manalmukarram18@gmail.com>
2025-01-11 06:47:08 +0530

    practised enums

---
+++ Enums.dart
@@ -0,0 +1,27 @@
+/*
+Exercise 2: Traffic Lights
+Create an enum called TrafficLight with values: Red, Yellow, Green.
+
+Write a function trafficLightMessage that:
+
+Takes a TrafficLight as input.
+Returns a string message:
+"Stop" for Red.
+"Ready" for Yellow.
+"Go" for Green.
+Test the function by passing each value of the enum.
+*/
+
+enum TrafficLights { red, yellow, green }
+
+void main() {
+  TrafficLights lights = TrafficLights.red;
+  switch (lights) {
+    case TrafficLights.red:
+      print('STOP');
+    case TrafficLights.yellow:
+      print('STOP');
+    case TrafficLights.green:
+      print('STOP');
+  }
+}
